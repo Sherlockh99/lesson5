bb8b5614e4339168f5d02e2ff8899688037b29f3
Sherlock <sherlockh989@gmail.com>
2022-12-17 23:09:13 +0200

    First commit

---
+++ app/src/main/java/com/sherlock/gb/android1/lesson5/MainActivity.java
@@ -0,0 +1,31 @@
+package com.sherlock.gb.android1.lesson5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        Button btnGreetings = findViewById(R.id.btnGreetings);
+        final EditText txtName = findViewById(R.id.textName);
+        final TextView txtGreetings = findViewById(R.id.textHello);
+        btnGreetings.setOnClickListener(view -> {
+            String name = txtName.getText().toString();
+            String sayHello = getString(R.string.say_hello) + name;
+            txtGreetings.setText(sayHello);
+        });
+    }
+}
